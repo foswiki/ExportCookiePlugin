bf906c7d3be6645915843dc8e57b0f8775bba9f3
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/ExportCookiePlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/ExportCookiePlugin.pm
+++ lib/Foswiki/Plugins/ExportCookiePlugin.pm
@@ -1,6 +1,6 @@
 #!/usr/local/bin/perl -wI.
 #
-# This script Copyright (c) 2008 Impressive.media 
+# This script Copyright (c) 2008 Impressive.media
 # and distributed under the GPL (see below)
 #
 # Based on parts of GenPDF, which has several sources and authors
@@ -14,9 +14,8 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
-
 
 package Foswiki::Plugins::ExportCookiePlugin;
 
@@ -24,23 +23,26 @@ use strict;
 
 use Assert;
 use HTTP::Cookies::Find;
-use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
+use vars
+  qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
 
 $VERSION = '$Rev: 12445$';
 
 $RELEASE = 'Dakar';
 
-$SHORTDESCRIPTION = 'Exports the value of a cookie into the Foswiki variable %EXPORTCOOKIE%';
+$SHORTDESCRIPTION =
+  'Exports the value of a cookie into the Foswiki variable %EXPORTCOOKIE%';
 $NO_PREFS_IN_TOPIC = 1;
 
 $pluginName = 'ExportCookiePlugin';
 
 sub initPlugin {
-    my( $topic, $web, $user, $installWeb ) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $Foswiki::Plugins::VERSION < 2.0 ) {
-        Foswiku::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if ( $Foswiki::Plugins::VERSION < 2.0 ) {
+        Foswiku::Func::writeWarning(
+            "Version mismatch between $pluginName and Plugins.pm");
         return 0;
     }
 
@@ -49,18 +51,17 @@ sub initPlugin {
 }
 
 sub _GETCOOKIEVALUE {
-    my($this, $params, $theTopic, $theWeb) = @_;
- 	my $query = Foswiki::Func::getCgiQuery();	
-	my $value = "";
-	my $CookieName = $Foswiki::cfg{Plugins}{ExportCookiePlugin}{CookieName};
+    my ( $this, $params, $theTopic, $theWeb ) = @_;
+    my $query      = Foswiki::Func::getCgiQuery();
+    my $value      = "";
+    my $CookieName = $Foswiki::cfg{Plugins}{ExportCookiePlugin}{CookieName};
 
-	foreach my $name ($query->cookie()) {
-		if($name eq $CookieName)
-		{
-			$value = $query->cookie('-name'=>$name);
-		}
- 	}
-	
-	return $value;
+    foreach my $name ( $query->cookie() ) {
+        if ( $name eq $CookieName ) {
+            $value = $query->cookie( '-name' => $name );
+        }
+    }
+
+    return $value;
 }
 1;
